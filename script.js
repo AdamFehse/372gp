f593dd1f94ffc7f523492e6a121454aa6227f862
arshawissinger <166645389+arshawissinger@users.noreply.github.com>
2025-04-29 15:52:16 -0700

    Update script.js
    removed (fs) functionality

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // script.js
 
-var fs = require('fs')
-
 // Wait until the page loads, then start our game  
 document.addEventListener('DOMContentLoaded', init);
 
@@ -13,16 +11,8 @@ let file;             // csv file with words
 
 // === 1) Initialization ===
 function init() {
-  var words;
-  file = fs.readFileSync('words.csv', {'encoding':'utf8'}, (err, data) => {
-    if (err){
-      console.error('Error reading file:', err);
-      return;
-    }
-    words = data.split(',');
-    selectedWord = words[Math.floor(Math.random()*100)];
-  });
-  
+  var words = data.split("unicorn,firework,puppy,banana,cupcake,rainbow,guitar,candy,balloon,pirate,dragon,cookie,sunshine,rocket,castle,puzzle,bunny,zebra,penguin,wizard,icecream,beach,robot,magic,sparkle,clown,marshmallow,kitten,fairy,apple,popcorn,dance,jelly,crayon,whistle,glitter,party,tiger,bubbles,treasure,skate,smile,pillow,crayon,book,flower,cloud,butterfly,donut,star,moon,pencil,school,train,friend,birthday,lunch,shoe,chair,window,blanket,toy,car,truck,bus,slide,swing,park,zoo,circus,story,house,family,baby,dog,cat,bird,fish,hat,glove,sock,snow,leaf,tree,grass,milk,juice,cake,lollipop,bed,night,morning,music,game,laugh,block,paint,color,bubble")
+  selectedWord = words[Math.floor(Math.random()*100)];
   guessedLetters = [];
   remainingLives = 6;
 
@@ -93,3 +83,4 @@ function showStatus(msg) {
   // TODO: grab <div id="status"> and set its textContent = msg
   // TODO (optional): add CSS classes like "error" or "success" for colors
 }
+
